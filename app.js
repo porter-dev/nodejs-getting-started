0b4ee87d68d2a9e767ee40d6f4bf722f7dd21ac8
Rudi MK <rudimk.cloud@gmail.com>
2024-04-04 14:57:38 +0200

    Added basic logging.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
 
+// Initialise morgan's middleware
+app.use(morgan("tiny"))
+
+// Root path handler
 app.get('/', (req, res) => {
   res.json({
     "path": "/",
@@ -9,12 +15,14 @@ app.get('/', (req, res) => {
   })
 })
 
+// Healthcheck handler
 app.get('/healthz', (req, res) => {
     res.json({
         "status": "OK"
     })
 })
 
+// Boot up Express
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
